dd73e4ae67817a1eeb23f72055981d88d9569054
DAMMES, NIKLAS <ndammes@protonmail.com>
2022-12-05 23:28:27 +0100

    new testcase

--- test_main.py
+++ test_main.py
@@ -5,5 +5,8 @@ def test_a():
     assert printBiggerNr(10, 5) == 10
 
 
-def tets_b():
+def test_b():
     assert printBiggerNr(5, 8) == 10
+
+def test_ab():
+    assert printBiggerNr(5, 5) == "a=b"
